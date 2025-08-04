2cbefbf59bfe3ddaf25b12c76979a4e5d27ffee4
neverx3gu <daehokwon@knu.ac.kr>
2025-08-04 21:36:53 +0900

    fix: residual branch의 마지막 Conv 레이어 in_channels를 ch에서 16으로 수정

--- models.py
+++ models.py
@@ -117,7 +117,7 @@ class UpsamplingBaselineNetwork(nn.Module):
                 nn.ReLU(),
                 nn.PixelShuffle(2) # 16 채널
             ])
-        residual_layers.append(nn.Conv2d(ch, channels, kernel_size=3, padding=1)) # 64 채널
+        residual_layers.append(nn.Conv2d(16, channels, kernel_size=3, padding=1)) # 3 채널
         self.residual_branch = nn.Sequential(*residual_layers)
 
         # 3. 업샘플링 커널 브랜치 (upsampling kernel branch)
